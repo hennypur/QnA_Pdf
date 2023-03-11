abee05f2fb65fcfb3deda6af835856e4eff6ed3f
Hennypur <85436361+hennypur@users.noreply.github.com>
2023-03-11 14:22:45 +0700

    Add files via upload

--- QA.py
+++ QA.py
@@ -1,7 +1,7 @@
+
 import streamlit as st
 from transformers import pipeline
 from PyPDF2 import PdfReader
-import io
 
 def load_pdf(file):
     pdf_reader = PdfReader(file)
@@ -19,11 +19,13 @@ def main():
     # Generate summary if file is uploaded
     if file is not None:
         # Read PDF content
-        text = load_pdf(file)
+        with st.spinner('Extracting text from PDF...'):
+            text = load_pdf(file)
 
         # Define question answering pipeline
-        choose_model = "bert-large-uncased-whole-word-masking-finetuned-squad"
-        qa = pipeline("question-answering", model=choose_model)
+        choose_model = "distilbert-base-cased-distilled-squad"
+        with st.spinner('Loading model...'):
+            qa = pipeline("question-answering", model=choose_model)
 
         # Define a function to ask and answer questions
         def ask_question(question, text, qa):
@@ -33,33 +35,32 @@ def main():
 
         # Define a list of example questions
         questions = [
-            "What is the settlement agreement about?",
-            "When was the Date of loss?",
-            "Who are Our Client?",
+            "Who is Our Client?",
             "What is Our Client's Gender?",
-            "What is Our Client's Date of Birth?",
-            "Who are the PERSON names in MEDICAL TREATMENTS?",
-            "When was the earliest Date of Treatment?",
-            "Did date of Treatments more than 30 days after Date of Loss?",
             "What are the most severe damage Our client's have?",
             "What are the mental, emotions, and other non-physical damage Our Client's have?"
         ]
 
         # Display example questions and answers
         st.write("Here are some example QnA of the PDF content:")
-        for question in questions:
-            answer = ask_question(question, text, qa)
-            st.write(f"Q: {question}")
-            st.write(f"A: {answer}\n")
+        with st.spinner('Answering example questions...'):
+            for question in questions:
+                answer = ask_question(question, text, qa)
+                st.write(f"Q: {question}")
+                st.write(f"A: {answer}\n")
 
         # Allow user to input custom question
         st.write("Enter your question below:")
         user_question = st.text_input("", "")
         if user_question:
-            answer = ask_question(user_question, text, qa)
-            st.write(f"Q: {user_question}")
-            st.write(f"A: {answer}\n")
+            with st.spinner('Answering your question...'):
+                answer = ask_question(user_question, text, qa)
+                st.write(f"Q: {user_question}")
+                st.write(f"A: {answer}\n")
 
 
 if __name__ == "__main__":
+    st.set_page_config(page_title='PDF QnA', page_icon=':books:')
+    st.title('PDF QnA')
+    st.write('This app allows you to ask questions about a PDF document.')
     main()
